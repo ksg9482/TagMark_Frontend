0f45d116328d3e78a4099cd3580fdda513fee425
ksg9482 <ksg930523@gmail.com>
2023-01-25 21:25:49 +0900

    배포 버그 수정

---
+++ src/defaultBookmark/defaultBookmark.ts
@@ -0,0 +1,40 @@
+export const defaultBookmark = [
+    {
+        id: 2,
+        url: 'https://www.tistory.com/',
+        tags: [
+            { id: 0, tag: '포털' },
+            { id: 3, tag: '티스토리' },
+            { id: 4, tag: '검색' },
+            { id: 5, tag: '블로그' }
+        ]
+    },
+    {
+        id: 1,
+        url: 'https://www.google.com/',
+        tags: [
+            { id: 0, tag: '포털' },
+            { id: 2, tag: '구글' },
+            { id: 4, tag: '검색' }
+        ]
+    },
+    {
+        id: 0,
+        url: 'https://www.naver.com/',
+        tags: [
+            { id: 0, tag: '포털' },
+            { id: 1, tag: '네이버' },
+            { id: 4, tag: '검색' },
+            { id: 5, tag: '블로그' }
+        ]
+    }
+];
+
+/*
+0 포털
+1 네이버 
+2 구글
+3 티스토리
+4 검색
+5 블로그
+*/
--- src/dumy/dumy-tags.ts
+++
@@ -1,6 +0,0 @@
-
-export const dumyTags = [
-    {id:11, name:'개발'},
-    {id:6, name:'프랑스'},
-    {id:4, name: '외국'},
-]
