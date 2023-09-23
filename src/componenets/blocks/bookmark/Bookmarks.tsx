42a286a724b13e96ba9a5d0f60b6bdd015093ea4
ksg9482 <ksg930523@gmail.com>
2023-09-23 21:20:05 +0900

    로컬 상태에서 북마크를 전부 삭제했을 때 발생하는 에러 조치.

--- src/componenets/blocks/bookmark/Bookmarks.tsx
+++ src/componenets/blocks/bookmark/Bookmarks.tsx
@@ -1,16 +1,23 @@
-import { Bookmark } from "../../../interface/bookmark"
-import BookmarkComponent from "./BookmarkComponent"
-import { BookmarksContainer } from "./style"
+import { Bookmark } from "../../../interface/bookmark";
+import BookmarkComponent from "./BookmarkComponent";
+import { BookmarksContainer } from "./style";
 
 const Bookmarks = (props: any) => {
-    const bookmarkView: Bookmark[] = props.bookmarkView
-    return (
-        <BookmarksContainer id="bookmarks">
-            {bookmarkView.map((bookmark: Bookmark) => (
-                <BookmarkComponent id={bookmark.id} bookmark={bookmark} key={bookmark.id} getTagBookmark={props.getTagBookmark} onBookmarkDelete={props.onBookmarkDelete} editSave={props.editSave}/>
-            ))}
-        </BookmarksContainer>
-    )
-}
+  const bookmarkView: Bookmark[] = props.bookmarkView;
+  return (
+    <BookmarksContainer id="bookmarks">
+      {bookmarkView.map((bookmark: Bookmark) => (
+        <BookmarkComponent
+          id={bookmark.id}
+          bookmark={bookmark}
+          key={bookmark.id}
+          getTagBookmark={props.getTagBookmark}
+          onBookmarkDelete={props.onBookmarkDelete}
+          editSave={props.editSave}
+        />
+      ))}
+    </BookmarksContainer>
+  );
+};
 
-export default Bookmarks
+export default Bookmarks;
--- src/utils/secure.ts
+++ src/utils/secure.ts
@@ -1,57 +1,60 @@
-import CryptoJS from 'crypto-js'
-import config from '../config'
+import CryptoJS from "crypto-js";
+import config from "../config";
 export const secure = () => {
-    const SECRET_KEY = config.CRYPTOJS_SECRET_KEY!;
-    const encrypt = (message:string) => {
-        const encrypted = CryptoJS.AES.encrypt(message, SECRET_KEY)
-        return encrypted.toString()
-    }
-    const decrypt = (data:string) => {
-        const decrypted = CryptoJS.AES.decrypt(data, SECRET_KEY)
-        return decrypted.toString(CryptoJS.enc.Utf8)
-    }
-    const setItem = (key:string, item:string) => {
-        localStorage.setItem(key,encrypt(item))
-    }
-    const getItem = (key:string) => {
-        const encrypted = localStorage.getItem(key)
-        if(!encrypted) {
-            return null
-        }
-        else {
-            return decrypt(encrypted)
-        }
-        
-    }
-    const removeItem = (key:string) => {
-        localStorage.removeItem(key)
-    }
-    const local = () => {
-        return {
-            setItem, getItem, removeItem
-        }
-    }
-    const encryptWrapper = (data:any) => {
-        return encrypt(data)
-    }
-    const decryptWrapper = (encryptStr:string) => {
-        try {
-            const result = decrypt(encryptStr)
-            if(!result) {throw false}
-            return result
-        } catch (error) {
-            return encryptStr;
-        }
-    }
+  const SECRET_KEY = config.CRYPTOJS_SECRET_KEY || "";
 
-
-    const wrapper = () => {
-        return {
-            encryptWrapper, decryptWrapper
-        }
+  const encrypt = (message: string) => {
+    const encrypted = CryptoJS.AES.encrypt(message, SECRET_KEY);
+    return encrypted.toString();
+  };
+  const decrypt = (data: string) => {
+    const decrypted = CryptoJS.AES.decrypt(data, SECRET_KEY);
+    return decrypted.toString(CryptoJS.enc.Utf8);
+  };
+  const setItem = (key: string, item: string) => {
+    localStorage.setItem(key, encrypt(item));
+  };
+  const getItem = (key: string) => {
+    const encrypted = localStorage.getItem(key);
+    if (!encrypted) {
+      return null;
+    } else {
+      return decrypt(encrypted);
     }
+  };
+  const removeItem = (key: string) => {
+    localStorage.removeItem(key);
+  };
+  const local = () => {
     return {
-        local, wrapper
+      setItem,
+      getItem,
+      removeItem,
+    };
+  };
+  const encryptWrapper = (data: any) => {
+    return encrypt(data);
+  };
+  const decryptWrapper = (encryptStr: string) => {
+    try {
+      const result = decrypt(encryptStr);
+      if (!result) {
+        throw false;
+      }
+      return result;
+    } catch (error) {
+      return encryptStr;
     }
-}
+  };
 
+  const wrapper = () => {
+    return {
+      encryptWrapper,
+      decryptWrapper,
+    };
+  };
+  return {
+    local,
+    wrapper,
+  };
+};
